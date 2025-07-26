27c1d5a57463988ca72dbdea7faec24b67658c45
Muhammad Aka Sahadi <makasahadi14@gmail.com>
2025-07-26 20:26:06 +0700

    Ada yg baru

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,10 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    return (
+      <div>
+        <h1>CIHUY</h1>
+      </div>
+    )
   </StrictMode>,
 )
